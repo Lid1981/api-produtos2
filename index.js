8c92a5427d8260a7fbef37b1eb5a3088fc5646ec
Lid1981 <lidramos1981@gmail.com>
2025-06-10 10:43:34 -0300

    Api finalizada

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = 4000;
 require('./src/models')
 const produtosRoutes = require('./src/routes/produtos')
 const usersRoutes = require('./src/routes/users')
